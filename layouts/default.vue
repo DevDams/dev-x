bfc8f99c857b7f7a050815d6615daf6a39075919
DevDams <ad.aimedesire@gmail.com>
2021-02-25 00:55:46 +0000

    project initialize

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="bg-gray-700">
+    <Nuxt />
+  </div>
+</template>
+
+<style>
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="h-screen">
+    <h1 class="text-white text-6xl font-bold">Hello World</h1>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
