4787754f28870e2bdefc599b161c3c77014c8b73
DevDams <ad.aimedesire@gmail.com>
2021-03-03 01:06:10 +0000

    Bug fixed

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,13 @@
 <template>
   <div>
-    <DarkModeBtn />
     <div class="corps bg-blue-400">
-      <div class="container m-auto flex h-screen items-center">
-        <h1 class="text uppercase text-3xl font-bold dark:text-black">
+      <div class="container relative m-auto flex items-center justify-center h-full z-50">
+        <DarkModeBtn class="absolute top-0" />
+        <h1 class="text uppercase text-3xl font-bold">
           Hi ! I am Aimé, a javascript and frontend web developer <br>from Ivory Coast
         </h1>
       </div>
-      <div class="wave_background">
+      <div class="wave_background z-10">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 150"><path fill="#63B3ED" fill-opacity="1" d="M0,64L80,80C160,96,320,128,480,128C640,128,800,96,960,80C1120,64,1280,64,1360,64L1440,64L1440,0L1360,0C1280,0,1120,0,960,0C800,0,640,0,480,0C320,0,160,0,80,0L0,0Z"></path></svg>
       </div>
     </div>
@@ -20,14 +20,29 @@ export default {}
 </script>
 
 <style>
+body {
+  background-color: #fff;
+  color: #0e3854;
+  transition: .3s all ease-in-out;
+}
+
+.dark-mode body {
+  background-color: #0e3854;
+  color: #c1e6ff;
+}
+
+.dark-mode .wave_background svg {
+  background: #0e3854;
+}
+
 .corps {
   height: 750px;
   position: relative;
-  background-color: #b9d4db;
+  background-color: #067eed;
 }
 
 .corps .text {
-  color: #de5b6d;
+  color: #f2f2f2;
 }
 
 .wave_background svg {
@@ -35,24 +50,10 @@ export default {}
   bottom: 0;
   background-color: #fff;
   transition: .3s all ease-in-out;
+  z-index: 1;
 }
 
 .wave_background svg path {
-  fill: #b9d4db;
-}
-
-body {
-  background-color: #fff;
-  color: rgba(0,0,0,0.8);
-  transition: .3s all ease-in-out;
-}
-
-.dark-mode body {
-  background-color: #091a28;
-  color: #ebf4f1;
-}
-
-.dark-mode .wave_background svg {
-  background: #091a28;
+  fill: #067eed;
 }
 </style>
