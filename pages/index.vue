f389abad97bf018b30b533c706e628e064316f1c
DevDams <ad.aimedesire@gmail.com>
2021-03-04 17:26:44 +0000

    start creating about sectioné

--- pages/index.vue
+++ pages/index.vue
@@ -10,15 +10,47 @@
             Create awesome interface and web application for customers if one of the thinks
             I like the most in my life. So if you have any idea of project, don't wait too long...
           </p>
-          <a href="#" class="text-xl text-white p-4 rounded-3xl aa">Let's work</a>
+          <a href="#" class="flex w-48 items-center justify-center text-xl text-white py-3 px-5 rounded-3xl aa">
+            Let's work
+            <span>
+              <svg width="40px" height="40px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M7 12L17 12L13 16M13 8L15 10" stroke="#000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+              </svg>
+            </span>
+          </a>
         </div>
         <img class="w-2/5" src="~/assets/svg/work_man.svg" alt="worker man illustration">
       </div>
       <div class="wave_background z-10">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 150"><path fill="#63B3ED" fill-opacity="1" d="M0,64L80,80C160,96,320,128,480,128C640,128,800,96,960,80C1120,64,1280,64,1360,64L1440,64L1440,0L1360,0C1280,0,1120,0,960,0C800,0,640,0,480,0C320,0,160,0,80,0L0,0Z"></path></svg>
+        <svg class="eeee" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 150"><path fill="#63B3ED" fill-opacity="1" d="M0,64L80,80C160,96,320,128,480,128C640,128,800,96,960,80C1120,64,1280,64,1360,64L1440,64L1440,0L1360,0C1280,0,1120,0,960,0C800,0,640,0,480,0C320,0,160,0,80,0L0,0Z"></path></svg>
       </div>
     </div>
-    <div class="h-screen"></div>
+    <!-- ===== About section ===== -->
+    <div class="h-screen about relative -mt-44">
+      <div class="container mx-auto">
+        <h1 class="text-6xl font-hairline text-right mr-32 flex justify-end">
+          <span>About me.</span> <span class="bar absolute bg-mygreen w-40 h-1 rounded-xl mt-20"></span>
+        </h1>
+        <div class="flex">
+          <div class="left_box w-1/3">
+            <img src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8cG9ydHJhaXR8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&w=1000&q=80" alt="">
+          </div>
+          <div class="right_box w-2/3 flex">
+            <div class="div1 w-1/2">
+              <h1>hello1</h1>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum maxime sed culpa dolor illo omnis neque nesciunt aliquid, dolore saepe doloremque earum, eveniet esse delectus eaque, ad voluptate corporis officia.</p>
+            </div>
+            <div class="div2 w-1/2 bg-mypink">
+              <div class="mt-52">
+                <h1>hello2</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid enim voluptatum obcaecati hic illo saepe, officiis veniam voluptatem consequatur totam minima eaque, nisi voluptas facilis at cum, dolor delectus! Doloribus.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
