f67edc5f93c42a7e665b126d9d598e6a8ce7ad12
DevDams <ad.aimedesire@gmail.com>
2021-03-10 17:17:57 +0000

    strat design contact page

--- pages/contacts/index.vue
+++ pages/contacts/index.vue
@@ -1,6 +1,18 @@
 <template>
   <div>
-      hello
+    <div class="contact_page">
+      <div class="mt-16 container mx-auto">
+        <div class="">
+          <h1 class="text-6xl text-mygreen font-semibold mt-32 ml-4">You have a project ?</h1>
+        </div>
+        <div class="w-2/3">
+          <p class="ml-4">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis similique voluptas saepe. Distinctio, error? Vel, ullam dolorum blanditiis eius, est necessitatibus nisi, officia alias qui perspiciatis at. Nihil, provident molestiae!
+          </p>
+        </div>
+        <div class="flex"></div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -9,5 +21,7 @@ export default {}
 </script>
 
 <style scoped>
-
+.bg-myblack {
+  width: 700px;
+}
 </style>
