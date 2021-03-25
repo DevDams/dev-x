63eafa525c2da19a3d12245fb2bde6ec515f7fa8
DevDams <ad.aimedesire@gmail.com>
2021-03-25 16:15:08 +0000

    design contact page

--- pages/contacts/index.vue
+++ pages/contacts/index.vue
@@ -1,16 +1,31 @@
 <template>
   <div>
-    <div class="contact_page">
+    <div class="contact_page bg-mywhite h-screen">
       <div class="mt-16 container mx-auto">
         <div class="">
-          <h1 class="text-6xl text-mygreen font-semibold mt-32 ml-4">You have a project ?</h1>
+          <h1 class="text-6xl text-mygreen font-semibold pt-32">You have a project ?</h1>
         </div>
-        <div class="w-2/3">
-          <p class="ml-4">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis similique voluptas saepe. Distinctio, error? Vel, ullam dolorum blanditiis eius, est necessitatibus nisi, officia alias qui perspiciatis at. Nihil, provident molestiae!
-          </p>
+        <div class="flex">
+          <div class="contact_text">
+            <p class="mt-8 w-1/2">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis similique voluptas saepe. Distinctio, error? Vel, ullam dolorum blanditiis eius, est necessitatibus nisi, officia alias qui perspiciatis at. Nihil, provident molestiae!
+            </p>
+            <h3 class="mt-8 text-3xl text-black font-semibold">Or find me on social network</h3>
+            <ul class="ml-12">
+              <li class="py-4"><img class="inline -mt-2" src="~/assets/icon/twitter-alt.svg" alt=""> Twitter : <a class="text-mygreen font-medium" href="https://twitter.com/dams9ix">@dams9ix</a></li>
+              <li class="py-4"><img class="inline -mt-2" src="~/assets/icon/instagram-alt.svg" alt=""> Instagram : <a class="text-mygreen font-medium" href="https://www.instagram.com/adam.9ix/">@adam.9ix</a></li>
+              <li class="py-4"><img class="inline -mt-2" src="~/assets/icon/behance.svg" alt=""> Behance : <a class="text-mygreen font-medium" href="https://behance.com/dams9ix">dams9ix</a></li>
+            </ul>
+            <h4 class="mt-12 text-2xl text-black font-semibold">Send me an email or call me :)</h4>
+            <p class="mt-4">
+              Email : ad.aimedesire@gmail.com <br>
+              Telephone : +225 07 78 81 21 11
+            </p>
+          </div>
+          <div class="form  w-1/2">
+            <h1>Fill this form</h1>
+          </div>
         </div>
-        <div class="flex"></div>
       </div>
     </div>
   </div>
