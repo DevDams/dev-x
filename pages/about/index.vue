91ee84d63c963c3611a7667633c586b30e8ffea0
DevDams <ad.aimedesire@gmail.com>
2021-04-28 23:18:13 +0000

    complete about page content

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -2,10 +2,16 @@
   <div id="target">
     <div data-scroll-container>
       <div data-scroll-section class="about_landing bg-myblue">
-        <h1 class="text-6xl font-bold text-center text-myfadegreen pt-32">WHO AM I</h1>
-        <div class="container about_me">
-          <div class="my_info">
-            <h1></h1>
+        <h1 class="text-6xl font-bold text-center text-myfadegreen pt-40">WHO AM I</h1>
+        <div class="about_me container mx-auto mt-12 flex">
+          <div class="my_info w-1/2 p-20">
+            <h1 class="text-myblack text-5xl font-semibold">I'm a human</h1>
+            <p class="text-mywhite">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed accusantium, fugit iste necessitatibus tempore facilis sequi blanditiis nisi repellat perferendis aliquam? Facilis accusantium quam ratione maxime quia temporibus laborum eum.
+            </p>
+          </div>
+          <div class="my_image w-1/2">
+            <img src="~/assets/svg/reading.svg" alt="">
           </div>
         </div>
       </div>
@@ -30,5 +36,10 @@ export default {
 <style scoped>
 .about_landing {
   height: 950px;
+}
+
+.about_me .my_image img {
+  width: 450px;
+  margin: auto;
 }
 </style>
